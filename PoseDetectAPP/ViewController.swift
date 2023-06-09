aa9aba9a5758694f2afafae2d2c92ec90433c6d2
taki-ser <rt12201311@gmail.com>
2023-06-09 11:23:44 +0900

    Initial Commit

---
+++ PoseDetectAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoseDetectAPP
+//
+//  Created by 滝瀬隆斗 on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
